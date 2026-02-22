5bedd7403a5a7fda0102f9885eda8421a58c35ec
owais815 <sultanowaismehmood@gmail.com>
2026-02-23 01:21:05 +0500

    Replace hardcoded JWT secret with environment variable for improved security in authentication middleware.

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -13,7 +13,7 @@ module.exports = (req,res,next)=>{
 
     try{
 
-        decodedToken = jwt.verify(token,'supersupersecretsecret');
+        decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
     }catch(err){
         err.statusCode = 500;
