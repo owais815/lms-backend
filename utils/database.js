68c34a20da2c2a20069e299893913cfe8c3b3a4b
owais815 <sultanowaismehmood@gmail.com>
2026-02-23 01:21:52 +0500

    Update database configuration import to use the new config structure for environment settings.

--- utils/database.js
+++ utils/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // Use environment-based configuration
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+const config = require('../config/config')[env];
 
 const sequelize = new Sequelize(config.database, config.username, config.password, {
     dialect: config.dialect,
